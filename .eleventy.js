d57dab7176605d4bd875f39ce94ba59693535979
nimblestart <nimble.start@gmail.com>
2022-05-10 11:13:17 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.addWatchTarget('./src/css/tailwind.css');
+  eleventyConfig.addPassthroughCopy({'./src/img': '.'});
+  return {
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: 'src',
+      output: 'public'
+    }
+  }
+}
